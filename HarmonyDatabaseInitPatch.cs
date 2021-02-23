d68e6851de741c5e78345808e724edd2a14ad142
Vit 'Alesseon' Vavra <alesseon1@gmail.com>
2021-02-23 22:02:14 +0100

    First version of the mod

---
+++ HarmonyDatabaseInitPatch.cs
@@ -0,0 +1,51 @@
+﻿using Harmony;
+
+namespace Alesseon.HarmonyDatabasePatch.LiquidBottleHandling
+{
+
+    [HarmonyPatch(typeof(GeneratedBuildings), "LoadGeneratedBuildings")]
+    public class LiquidBottlesBuildingsPatch
+    {
+        private static void Postfix()
+        {
+            //LiquidBottler strings
+            Strings.Add(new string[] {
+                    "STRINGS.BUILDINGS.PREFABS.ALESSEON.LIQUIDBOTTLER.NAME",
+                    "Liquid Bottler"
+                });
+            Strings.Add(new string[] {
+                    "STRINGS.BUILDINGS.PREFABS.ALESSEON.LIQUIDBOTTLER.DESC",
+                    "Allow Duplicants to fetch bottled liquids for delivery to buildings." //"This bottler station has access to: {Liquids}"
+				});
+            Strings.Add(new string[] {
+                    "STRINGS.BUILDINGS.PREFABS.ALESSEON.LIQUIDBOTTLER.EFFECT",
+                    "Automatically stores piped <link=\"ELEMENTSLIQUID\">Liquid</link> into bottles for manual transport." //"Liquid Available: {Liquids}"
+				});
+            //LiquidBottleEmptier strings
+            Strings.Add(new string[] {
+                    "STRINGS.BUILDINGS.PREFABS.ALESSEON.LIQUIDBOTTLEEMPTIER.NAME",
+                    "Liquid bottle emptier"
+                });
+            Strings.Add(new string[] {
+                    "STRINGS.BUILDINGS.PREFABS.ALESSEON.LIQUIDBOTTLEEMPTIER.DESC",
+                    "Allows emptying bottles directly to the pipe system." //"This bottler station has access to: {Liquids}"
+				});
+            Strings.Add(new string[]{
+                    "STRINGS.BUILDINGS.PREFABS.ALESSEON.LIQUIDBOTTLEEMPTIER.EFFECT",
+                    "Automatically empties <link=\"ELEMENTSLIQUID\">Liquid</link> from bottles for pipe transport."
+            });
+            ModUtil.AddBuildingToPlanScreen("Plumbing", Building.Config.LiquidBottlerConfig.ID);
+            ModUtil.AddBuildingToPlanScreen("Plumbing", Building.Config.LiquidBottleEmptierConfig.ID);
+        }
+    }
+
+    [HarmonyPatch(typeof(Db), "Initialize")]
+    public class LiquidBottlerDbPatch
+    {
+        private static void Postfix()
+        {
+            Db.Get().Techs.TryGet("ImprovedLiquidPiping")?.unlockedItemIDs.Add(Building.Config.LiquidBottlerConfig.ID);
+            Db.Get().Techs.TryGet("ImprovedLiquidPiping")?.unlockedItemIDs.Add(Building.Config.LiquidBottleEmptierConfig.ID);
+        }
+    }
+}
---
+++ LiquidBottlerConfig.cs
@@ -0,0 +1,81 @@
+﻿using TUNING;
+using UnityEngine;
+
+namespace Alesseon.Building.Config
+{
+    public class LiquidBottlerConfig : IBuildingConfig
+    {
+        public const string ID = "Alesseon.LiquidBottler";
+        //TODO add checks for wrong element inside LiquidBottler
+        public static readonly SimHashes[] enabledElements = new SimHashes[] { SimHashes.Water, SimHashes.DirtyWater, SimHashes.SaltWater };
+
+        public override BuildingDef CreateBuildingDef()
+        {
+
+            int width = 3;
+            int height = 2;
+            string anim = "gas_bottler_kanim";
+            int hitpoints = 30;
+            float construction_time = 30f;
+            float[] tier = BUILDINGS.CONSTRUCTION_MASS_KG.TIER4;
+            string[] raw_METALS = MATERIALS.ALL_METALS;
+            float melting_point = 800f;
+            BuildLocationRule buildLocationRule = BuildLocationRule.OnFloor;
+
+            BuildingDef buildingDef = 
+                BuildingTemplates.CreateBuildingDef(
+                    ID,
+                    width,
+                    height,
+                    anim,
+                    hitpoints,
+                    construction_time,
+                    tier,
+                    raw_METALS,
+                    melting_point,
+                    buildLocationRule,
+                    BUILDINGS.DECOR.PENALTY.TIER1,
+                    NOISE_POLLUTION.NOISY.TIER0,
+                    0.2f
+                )
+            ;
+            buildingDef.InputConduitType = ConduitType.Liquid;
+            buildingDef.UtilityInputOffset = new CellOffset(0, 0);
+            buildingDef.ViewMode = OverlayModes.LiquidConduits.ID;
+            buildingDef.Floodable = false;
+            buildingDef.Entombable = true;
+            buildingDef.AudioCategory = "HollowMetal";
+            buildingDef.DefaultAnimState = "on";
+            return buildingDef;
+        }
+
+        public override void ConfigureBuildingTemplate(GameObject go, Tag prefab_tag)
+        {
+            Prioritizable.AddRef(go);
+            Storage storage = go.AddOrGet<Storage>();
+            storage.SetDefaultStoredItemModifiers(Storage.StandardSealedStorage);
+            storage.showDescriptor = true;
+            storage.storageFilters = STORAGEFILTERS.LIQUIDS;
+            storage.capacityKg = 300f;
+            storage.allowItemRemoval = true;
+            storage.showInUI = true;
+            go.AddOrGet<DropAllWorkable>();
+
+            GasBottler liquidBottler = go.AddOrGet<GasBottler>();
+            liquidBottler.storage = storage;
+            liquidBottler.workTime = 9f;
+            ConduitConsumer conduitConsumer = go.AddOrGet<ConduitConsumer>();
+            conduitConsumer.storage = storage;
+            conduitConsumer.conduitType = ConduitType.Liquid;
+            conduitConsumer.ignoreMinMassCheck = true;
+            conduitConsumer.capacityKG = storage.capacityKg;
+            conduitConsumer.keepZeroMassObject = false;
+        }
+
+        public override void DoPostConfigureComplete(GameObject go)
+        {
+            BuildingTemplates.DoPostConfigure(go);
+        }
+    }
+
+}
